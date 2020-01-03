eccf18ffc76d3c09cc80ac1815173d1ad17474e4
zhanghongcun <zhanghongcun@bytedance.com>
2020-01-03 16:48:03 +0800

    first commit

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package testbuild
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("hello")
+	fmt.Println(getCurrentFilePath())
+	fmt.Println("success")
+}
+
+func getCurrentFilePath() string {
+	_, filePath, _, _ := runtime.Caller(1)
+	return filePath
+}
